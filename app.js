0762a9a3c3f9d7ba46a810d433e30143527b2c8e
VolZa <vvolza@gmail.com>
2023-07-04 08:07:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { tasksRouter } = require('./routes/tasks');
- app.use("/tasks", tasksRouter);
+
+app.use("/tasks", tasksRouter);
 
 module.exports = app;
